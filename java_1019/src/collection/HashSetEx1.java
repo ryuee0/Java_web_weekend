bfd651851c62bb3b2e1edac35b64c3d96e1a9984
ryuee0 <yj.ryuee@gmail.com>
2024-10-20 16:15:59 +0900

    java_1019

---
+++ java_1019/src/collection/HashSetEx1.java
@@ -0,0 +1,14 @@
+package collection;
+
+import java.util.HashSet;
+
+// HashSet 만들고 3개 입력한다음에 중복이면은 다른값 들어가도록
+public class HashSetEx1 {
+	public static void main(String[] args) {
+		HashSet<String> hashSet = new HashSet<>();
+		
+		
+		
+	}
+	
+}
