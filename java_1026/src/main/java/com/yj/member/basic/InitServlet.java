06aa58c774335f8d196ff16e8fa05e1879312dad
ryuee0 <yj.ryuee@gmail.com>
2024-10-26 15:59:24 +0900

    java_1026

---
+++ java_1026/src/main/java/com/yj/member/basic/InitServlet.java
@@ -0,0 +1,23 @@
+package com.yj.member.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(name = "initServlet", urlPatterns = "/init")
+public class InitServlet extends HttpServlet{
+	private String message;
+	
+	@Override
+	public void init() throws ServletException {
+		message = "initServlet";
+		
+		System.out.println(message);
+		System.out.println("----");
+		
+	}
+	
+	
+	
+	
+}
